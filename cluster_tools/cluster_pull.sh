bc3235bfff175cf12ba67c0055d616d24492a10f
yukimasano <yuki.m.asano@gmail.com>
2017-09-11 12:08:54 +0000

    deleted unnecessary files

--- cluster_tools/cluster_pull.sh
+++
@@ -1,1 +0,0 @@
-rsync -tv -r kolb@aix.pik-potsdam.de:/home/kolb/Divest_Experiments/divestdata/* ./divestdata/ || rsync -tv -r kolb@cluster:/home/kolb/Divest_Experiments/divestdata/* ./divestdata/
--- cluster_tools/cluster_push.sh
+++
@@ -1,1 +0,0 @@
-rsync -t -r --copy-links --exclude=divestdata * kolb@aix.pik-potsdam.de:/home/kolb/Savings_Experiments/ || rsync -t -r --copy-links --exclude=divestdata * kolb@cluster:/home/kolb/Savings_Experiments/
